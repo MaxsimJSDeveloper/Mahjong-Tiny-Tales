0cf083a30cb7407c3fc63b68e63e238a1fe75730
pilipchu <114575426+pilipchu@users.noreply.github.com>
2024-08-26 13:11:10 +0300

    how-to-play

--- src/js/mob-menu.js
+++ src/js/mob-menu.js
@@ -2,6 +2,7 @@
   const mobileMenu = document.querySelector('.mobile-menu-div');
   const openMenuBtn = document.querySelector('.js-open-menu');
   const closeMenuBtn = document.querySelector('.js-close-menu');
+  const list = document.querySelector('.mobile-menu-nav');
 
   const openMobileMenu = () => {
     mobileMenu.classList.toggle('is-close');
@@ -14,6 +15,8 @@
     openMenuBtn.classList.toggle('is-hidden');
     closeMenuBtn.classList.toggle('is-hidden');
   };
+
+  list.addEventListener('click', closeModileMenu);
 
   openMenuBtn.addEventListener('click', openMobileMenu);
 
